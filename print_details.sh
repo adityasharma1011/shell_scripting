99b685560b6ff67651e5bad1c323e7a25c5c2a6d
adityasharma1011 <kumar.adityasharma1227@gmail.com>
2021-11-17 19:53:12 -0500

    added my first commit

---
+++ print_details.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is my first shell script
+# Author: n0123456
+
+echo "========================================"
+date
+echo "hello world, I am learning shell scripts"
+echo "========================================"
