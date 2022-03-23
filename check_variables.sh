aff162e91ae27f580e6625dbab867f4c7899b716
adityasharma1011 <85143872+adityasharma1011@users.noreply.github.com>
2022-03-23 15:02:21 -0400

    Add files via upload

---
+++ check_variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# -------------------------------- #
+# Author: Kumar Aditya             #
+# Decription: learning variables   #
+# Created on: 21st March, 2022     #
+# -------------------------------- #
+
+
+## Variables definations
+COLLEGE="Stevens College"
+NAME="John"
+COURSE="Operating systems"
+
+## Starting of the script
+echo "Please find my details"
+echo "----------------------"
+echo "College = $COLLEGE "
+echo "Name    = $NAME "
+echo "Course  = $COURSE "
+echo "---------------------"
