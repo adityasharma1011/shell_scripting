12b7e569a4359f0a4ba720b7d41010bd97d04aec
adityasharma1011 <85143872+adityasharma1011@users.noreply.github.com>
2022-12-08 08:53:21 -0500

    Create loops_6.sh

---
+++ loops_6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Author: Manu Sharma
+#Description
+
+## Variable definations
+COUNTER=1
+NUM="$1"
+
+
+# Starting of while loop
+while [ $COUNTER -le 10 ]
+do
+	RESULT=$((NUM * COUNTER))
+        echo "$NUM * $COUNTER => $RESULT"	
+
+	COUNTER=$((COUNTER + 1)) 
+done
