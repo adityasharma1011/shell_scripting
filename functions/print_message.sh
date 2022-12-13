56ac19f0c0cbeb1a3558f7c7ef97f12fc74eeee9
adityasharma1011 <kumar.adityasharma1227@gmail.com>
2022-12-13 16:42:23 -0500

    modified scripts

---
+++ functions/print_message.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Author: n0123456
+#Description: functions 
+#Date: 13th April
+
+
+print_message()
+{
+echo "======================="
+echo "     hello world       "
+echo "======================="
+date
+}
+
+
+# Execution starts here 
+print_message
+
